929e0fe4822b234cd4d61c7afef185e552c0ee42
PAK0BAH <s.2544216@yandex.ru>
2025-06-24 20:16:14 +0300

    реализован базовый функционал

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,109 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import {Todos} from "./api/todos.ts";
+import * as React from "react";
+
+interface ITodo {
+    "id": number,
+    "text": string,
+    "completed": boolean,
+    // "createdAt": string
+}
+
+interface ITodoProps extends ITodo{
+    handleUpd: () => {}
+}
+
+function Todo({id, text, completed, handleUpd}: ITodoProps){
+    const [textTodo, setTextTodo] = useState(text)
+    const [editButton, setEditButton] = useState(false)
+    const [completedTodo, setCompletedTodo] = useState(completed)
+
+
+    const handleEditTitle = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (editButton) {
+            (async () => {
+                await Todos.updTodo(id, textTodo, completed)
+            })()
+            handleUpd()
+        }
+        setEditButton(prev => !prev)
+    }
+
+    const handleChangeCompeted = async () => {
+        const newCompleted = !completedTodo;
+        setCompletedTodo(newCompleted);
+        await Todos.updTodo(id, textTodo, newCompleted);
+        handleUpd()
+    }
+
+    const handleDeleteTodo = async () => {
+        await Todos.deleteTodo(id)
+        handleUpd()
+    }
+
+    return(<div className={'Todo | flex'}>
+        <form className={'editTitle | '} onSubmit={handleEditTitle}>
+            <button>
+                {editButton ? '✔' : '✐'}
+            </button>
+            <input className={editButton ? 'border' : ''}
+                   value={textTodo}
+                   onChange={e => setTextTodo(e.target.value)}
+                   disabled={!editButton}
+            />
+        </form>
+        <input type={'checkbox'}
+               checked={completedTodo}
+               onChange={handleChangeCompeted}
+        />
+        <button onClick={handleDeleteTodo}>✖️</button>
+    </div>)
+}
+
 
 function App() {
+    const [todos, setTodos] = useState<ITodo[]>([])
+    const [newTodoText, setNewTodoText] = useState('')
 
-    return (<>
-        123
-    </>)
+    const handleUpd = async () => {
+        const res = await Todos.getTodos(1, 10)
+        const todos = await res.json()
+        setTodos(todos.data)
+    }
+
+    const handleNewTodo= async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        await Todos.newTodo(newTodoText)
+        setNewTodoText('')
+        handleUpd()
+    }
+
+    useEffect(() => {
+        handleUpd()
+    }, []);
+
+    return (<div>
+        <form onSubmit={handleNewTodo}>
+            <input className={'border'}
+                   value={newTodoText}
+                   onChange={e => setNewTodoText(e.target.value)}
+            />
+            <button>new</button>
+        </form>
+        <button onClick={handleUpd}>upd</button>
+        <ul>
+            {todos.map((t) => (
+                <li key={t.id}>
+                    <Todo id={t.id}
+                          text={t.text}
+                          completed={t.completed}
+                          handleUpd={handleUpd}/>
+                </li>
+            ))}
+        </ul>
+    </div>)
 }
 
 export default App
---
+++ front/src/api/todos.ts
@@ -0,0 +1,31 @@
+export class Todos {
+    static async getTodos(page: number, limit: number){
+        return await fetch(`http://localhost:3001/todos?page=${page}&limit=${limit}`)
+    }
+
+    static async newTodo(text: string){
+        return await fetch(`http://localhost:3001/todos`,{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({text: text})
+        })
+    }
+
+    static async updTodo(id: number, text: string, completed: boolean){
+        const strId = id.toString() // на всякий случай
+        return await fetch(`http://localhost:3001/todos/${strId}`,{
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({text: text, completed: completed})
+        })
+    }
+
+    static async deleteTodo(id: number){
+        const strId = id.toString() // на всякий случай
+        return await fetch(`http://localhost:3001/todos/${strId}`,{
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+
+}
