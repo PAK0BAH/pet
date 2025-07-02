7ab9a05c3821ccabdfbfcd3f229f9cc9a6ce71a2
PAK0BAH <s.2544216@yandex.ru>
2025-07-02 19:19:33 +0300

    husky test5

--- front/src/components/Limit.tsx
+++ front/src/components/Limit.tsx
@@ -15,13 +15,10 @@ export default function Limit() {
             black: store.limit !== index,
         });
 
-    const test 9000
-
     const handleLimit = (limit: number) => {
         dispatch(changeLimit(limit));
         dispatch(changePage(1));
         dispatch(fetchData());
-        console.log(test);
     };
 
     return (
